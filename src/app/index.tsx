81d3e4e6a97f891eaf2edf0ec9365b1935b65e0a
MalSolt <freelance99@bk.ru>
2022-05-14 00:19:11 +0300

    add base settings

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { CalendarPage } from 'pages'
+import styles from './index.module.scss'
+
+export const App = () => {
+  return (
+    <div className={styles.app}>
+      <CalendarPage />
+    </div>
+  )
+}
